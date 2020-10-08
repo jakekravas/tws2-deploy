3454521d64fa1f093bd3f0dbd7796eda8d57db93
Jake Kravas <jakekravas7@gmail.com>
2020-10-08 06:03:38 -0700

    att

--- config/db.js
+++ config/db.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require('sequelize');
 
 module.exports = new Sequelize(
-  // "bulkmatic", // database
-  "d8gvbh0ij9qecq", // database
-  // "bmtDbAdmin187", // user
-  "swuuenwwpcogjh", // user
-  // "39qsYf73IJbakP3Ql5Oc", // password
-  "f7782b6c2576db4e7273dbde1b413bac234046f9170a3fe2ba3cb09642a4690c", // password
+  "bulkmatic", // database
+  // "d8gvbh0ij9qecq", // database
+  "bmtDbAdmin187", // user
+  // "swuuenwwpcogjh", // user
+  "39qsYf73IJbakP3Ql5Oc", // password
+  // "f7782b6c2576db4e7273dbde1b413bac234046f9170a3fe2ba3cb09642a4690c", // password
   {
-    // host: "bulkmatic-postgres-dev.chsi0luwnscx.us-east-2.rds.amazonaws.com",
-    host: "ec2-3-210-255-177.compute-1.amazonaws.com",
+    host: "bulkmatic-postgres-dev.chsi0luwnscx.us-east-2.rds.amazonaws.com",
+    // host: "ec2-3-210-255-177.compute-1.amazonaws.com",
     dialect: "postgres",
     // schema: "tw_scheduler",
     operatorsAliases: false,
