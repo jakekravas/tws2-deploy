5e40fcecca8548e3cf8fec266a65de01bb111481
Jake Kravas <jakekravas7@gmail.com>
2020-10-08 05:46:28 -0700

    s

--- config/db.js
+++ config/db.js
@@ -1,22 +1,23 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get("mongoURI");
+const { Sequelize } = require('sequelize');
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    });
+module.exports = new Sequelize(
+  // "bulkmatic", // database
+  "d8gvbh0ij9qecq", // database
+  // "bmtDbAdmin187", // user
+  "swuuenwwpcogjh", // user
+  // "39qsYf73IJbakP3Ql5Oc", // password
+  "f7782b6c2576db4e7273dbde1b413bac234046f9170a3fe2ba3cb09642a4690c", // password
+  {
+    // host: "bulkmatic-postgres-dev.chsi0luwnscx.us-east-2.rds.amazonaws.com",
+    host: "ec2-3-210-255-177.compute-1.amazonaws.com",
+    dialect: "postgres",
+    // schema: "tw_scheduler",
+    operatorsAliases: false,
 
-    console.log("MongoDB Connected...");
-  } catch (err) {
-    console.error(err.message);
-    // Exit process with failure
-    process.exit(1);
-  }
-}
-
-module.exports = connectDB;
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+});
