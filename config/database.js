a2e719b4b3d2e21b2ec241afb77e0e651a2f06ba
Jake Kravas <jakekravas7@gmail.com>
2020-10-22 10:29:15 -0700

    swapped databases

--- config/database.js
+++ config/database.js
@@ -1,21 +1,19 @@
 const { Sequelize } = require('sequelize');
 
 module.exports = new Sequelize(
-  // "bulkmatic", // database
-  "BtcTws", // database
-  // "bmtDbAdmin187", // user
-  "TwsApp", // user
-  // "39qsYf73IJbakP3Ql5Oc", // password
-  "T@nkw@sh!", // password
+  "bulkmatic", // database
+  // "BtcTws", // database
+  "bmtDbAdmin187", // user
+  // "TwsApp", // user
+  "39qsYf73IJbakP3Ql5Oc", // password
+  // "T@nkw@sh!", // password
   {
-    // host: "bulkmatic-postgres-dev.chsi0luwnscx.us-east-2.rds.amazonaws.com",
+    host: "bulkmatic-postgres-dev.chsi0luwnscx.us-east-2.rds.amazonaws.com",
     // host: "localhost",
     // host: "SQL_DB_Listener_DEV",
-    // port: "5000",
-    host: "SQL_DB_Listener_DEV",
-    // dialect: "postgres",
-    dialect: "mssql",
-    // schema: "tw_scheduler",
+    dialect: "postgres",
+    // dialect: "mssql",
+    schema: "tw_scheduler",
     operatorsAliases: false,
 
     pool: {
